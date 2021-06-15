043402f3547c20ffaac62c2c741193497da5ba1f
Sam Neus <sneus@nd.edu>
2021-06-15 15:51:51 -0400

    Rough calculator created

--- calculator.py
+++ calculator.py
@@ -8,6 +8,33 @@
 def calculator(lengthDict, percentList):
     score = 0
     
+    # Checks if there is a valid time
+    if lengthDict == None:
+        return None
+    elif lengthDict["Completionist"]["Time"] == None:
+        return None
 
+    # Adjusts the time value
+    if lengthDict["Completionist"]["Format"] == "Hours":
+        score = lengthDict["Completionist"]["Time"] * 60
+    else:
+        score = lengthDict["Completionist"]["Time"]
 
-    return score
+    # Adds in the score for achievements
+    if percentList != None:
+        multiplier = 100 / (sum(percentList) / len(percentList))
+
+        # 30000 was chosen after testing to match up with desired scores for certain games
+        score = int((100 * score * multiplier) / 30000)
+    else:
+        # 7500 was chosen to account for the difference in games without achievements
+        score = int((100 * score) / 7500)
+
+     
+    
+
+    # Some games can go way over 100, but the calculator gets more difficult to use at higher levels
+    if score > 100: 
+        return 101
+    else:
+        return int(score)
