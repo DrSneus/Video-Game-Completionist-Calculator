010fb3f01f051d0631d6e15bc71cc04e71cf6916
Sam Neus <sneus@nd.edu>
2021-06-16 20:07:21 -0400

    Allowed for user confirmation

--- main.py
+++ main.py
@@ -10,25 +10,36 @@ import calculator
 
 # Given a game's app id, will find and return a dictionary of different average completion lengths
 def main():
-    # Defining variables
+    hltbID = None
     print("Please provide the name of a game:")
     game = input()
+    while not hltbID:
+        # ID finding
+        hltbInfo = hltbscraper.findHLTBAppID(game)
+        steamID = steamscraper.findSteamAppID(game)
 
-    # ID finding
-    steamID = steamscraper.findSteamAppID(game)
-    hltbID = hltbscraper.findHLTBAppID(game)
+        if not hltbID:
+            print(f"The title \'{game}\' could not be found on How Long To Beat")
+            print("Please provide the game's title again\n")
+            game = input()
+        else:
+            print(f"The title found was {hltbInfo[0]}")
+            print("If this is correct, press Enter. Otherwise please provide the game's title again.")
+            game = input()
+            if game != "":
+                hltbID = None
+            else:
+                hltbID = hltbInfo[1]
+
+        
 
     # Data finding
+    lengthDict = hltbscraper.findLength(hltbID)
     percentList = steamscraper.findPercents(steamID)
-
-    if hltbID:
-        lengthDict = hltbscraper.findLength(hltbID)
-    else:
-        print(f"The title \'{game}\' could not be found on How Long To Beat")
-        return None
 
     # Results
     calculator.tableMaker(lengthDict, percentList)
+    calculator.descriptionMaker(lengthDict, percentList)
 
 # Main Execution
 if __name__ == '__main__':
